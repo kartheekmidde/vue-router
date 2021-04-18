1e980587fabcc4bd8144c52c85697ab4babbab84
Kartheek Midde <kartheek.midde@clearlink.com>
2021-04-17 23:54:34 -0600

    Fetching data from APIs

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading jobs...</p>
   </div>
 </template>
 
@@ -12,12 +17,19 @@ export default {
   name: "Jobs",
   data() {
     return {
-      jobs: [
-          { title: 'UX Developer', id: 1, details: 'test' },
-          { title: 'Web Developer', id: 2, details: 'test' },
-          { title: 'Vue Developer', id: 3, details: 'test' },
-        ]
+      // jobs: [
+      //     { title: 'UX Developer', id: 1, details: 'test' },
+      //     { title: 'Web Developer', id: 2, details: 'test' },
+      //     { title: 'Vue Developer', id: 3, details: 'test' },
+      //   ]
+      jobs: []
     }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+        .then(res =>  res.json() )
+        .then(data => { this.jobs = data })
+        .catch(err => console.log(err.message));
   }
 }
 </script>
