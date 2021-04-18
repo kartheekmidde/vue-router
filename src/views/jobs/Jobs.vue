eeb536f34330748403d952abfef996a64702aba8
Kartheek Midde <kartheek.midde@clearlink.com>
2021-04-17 22:46:49 -0600

    Adding additional routes

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jobs",
+  data() {
+    return {
+      jobs: [
+          { title: 'UX Developer', id: 1, details: 'test' },
+          { title: 'Web Developer', id: 2, details: 'test' },
+          { title: 'Vue Developer', id: 3, details: 'test' },
+        ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
